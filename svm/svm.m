d5c5f0c86b53685ffdafc68ceaa0b4617d8e09a7
Georgios Koloventzos <g.koloventzos@gmail.com>
2015-10-15 03:27:01 -0400

    problem 4

--- svm/svm.m
+++ svm/svm.m
@@ -16,51 +16,70 @@ end
 kkk={'linear','poly','rbf'};
 load('mydata.mat');
 ind = crossvalind('Kfold', 100, 2);
-xtr = X(ind ==1,:)
-xtest = X(ind ==2,:)
-ytr = Y(ind ==1,:)
-ytest = Y(ind ==2,:)
-nsv = zeros(100);
-%alpha = zeros(10,50);
-bias = zeros(100);
-%predY = zeros(10,50);
-lerr = zeros(100);
-perr = zeros(ct*pt,1);
-psvn = zeros(ct*pt,1);
-pbias = zeros(ct*pt,1);
-rerr = zeros(ct*pt,1);
-rsvn = zeros(ct*pt,1);
-rbias = zeros(ct*pt,1);
+xtr = X(ind ==1,:);
+xtest = X(ind ==2,:);
+ytr = Y(ind ==1,:);
+ytest = Y(ind ==2,:);
+lknsv = zeros(ct);
+lkbias = zeros(ct);
+lkerr = zeros(ct);
+pksvn = zeros(1,ct*pt);
+pkbias = zeros(1,ct*pt);
+rksvn = zeros(1,ct*pt);
+rkbias = zeros(1,ct*pt);
 for indi = 1:length(kkk)
     switch kkk{indi}
-        %case 'linear'
-        %    for c = 1:100
-        %        [nsv(c),alpha,bias(c)] = svc(xtr,ytr,'linear',c);
-        %        %predY = svcoutput(xtr,ytr,xtest,'linear',alpha,bias(c));
-        %        lerr(c) = svcerror(xtr,ytr,xtest,ytest,'linear',alpha,bias(c))
-        %    end
+        case 'linear'
+            for c = 1:ct
+                [lknsv(c),alpha,lkbias(c)] = svc(xtr,ytr,'linear',c);
+        %        %predY = svcoutput(xtr,ytr,xtest,'linear',alpha,lkbias(c));
+                lkerr(c) = svcerror(xtr,ytr,xtest,ytest,'linear',alpha,lkbias(c))
+        %        [lrnsv(c),alpha,lrbias(c)] = svr(xtr,ytr,'linear',c);
+        %        lrerr(c) = svrerror(xtr,xtest,ytest,'linear',alpha,lrbias(c))
+            end
+            figure
+            plot (lkerr);
+            ylabel('Error')
+            xlabel('C upper bound')
+            print -depsc linear.eps;
         case 'poly'
             n=1;
             for c = 1:ct
                 for p=1:pt
                     p1 = p;
-                    [pnsv(n),alpha,pbias(n)] = svc(xtr,ytr,'poly',c);
-                    %predY = svcoutput(xtr,ytr,xtest,'poly',alpha,bias(c));
-                    perr(n) = svcerror(xtr,ytr,xtest,ytest,'poly',alpha,pbias(n))
+                    [pksvn(n),alpha,pkbias(n)] = svc(xtr,ytr,'poly',c);
+                    %predY = svcoutput(xtr,ytr,xtest,'poly',alpha,pkbias(c));
+                    pkerr(p,c) = svcerror(xtr,ytr,xtest,ytest,'poly',alpha,pkbias(n));
+                    %[prnsv(n),alpha,prbias(n)] = svr(xtr,ytr,'linear',c);
+                    %lrerr(n) = svrerror(xtr,xtest,ytest,'linear',alpha,prbias(n))
                     n = n+1;
                 end
-            end            
+            end
+            figure
+            surf(pkerr)
+            ylabel('Dimension')
+            xlabel('C upper bound')
+            zlabel('Error')
+            print -depsc polynomial.eps;
         case 'rbf'
             n=1;
             for c = 1:ct
                 for p=1:pt
                     p1 = p;
-                    [rnsv(n),alpha,rbias(n)] = svc(xtr,ytr,'rbf',c);
-                    %predY = svcoutput(xtr,ytr,xtest,'rbf',alpha,bias(c));
-                    rerr(n) = svcerror(xtr,ytr,xtest,ytest,'rbf',alpha,rbias(n))
+                    [rknsv(n),alpha,rkbias(n)] = svc(xtr,ytr,'rbf',c);
+                    %predY = svcoutput(xtr,ytr,xtest,'rbf',alpharp,bias(c));
+                    rkerr(p,c) = svcerror(xtr,ytr,xtest,ytest,'rbf',alpha,rkbias(n));
+                    %[rrnsv(n),alpha,rrbias(n)] = svr(xtr,ytr,'linear',c);
+                    %rrerr(n) = svrerror(xtr,xtest,ytest,'linear',alpha,rrbias(n))
                     n=n+1;
                 end
             end
+            figure
+            surf(rkerr)
+            ylabel('Sigma')
+            xlabel('C upper bound')
+            zlabel('Error')
+            print -depsc rbf.eps;
     end
 end
 end
